d8f1f5920d1fe01992335ef35752ab3581a74452
virginashmi15 <65062793+virginashmi15@users.noreply.github.com>
2020-05-09 06:33:24 -0700

    Add files via upload

---
+++ custom.sql
@@ -0,0 +1,39 @@
+SELECT * FROM customer1.candidats
+ 
+ CREATE TABLE candidats 
+ (
+
+ (
+id int(11) primary key
+candidates_name varchar(45) 
+candidates_age int(11) 
+candidates_mail varchar(45) 
+candidates_gender varchar(45)
+
+values(1001,virgin,22,female),
+)
+ (
+  id int(11)  primary key
+candidates_name varchar(45) 
+candidates_age int(11) 
+candidates_mail varchar(45) 
+candidates_gender varchar(45)
+
+values(1002,anusha,22,female),
+ 
+ ) 
+ (
+  id int(11) PK 
+candidates_name varchar(45) 
+candidates_age int(11) 
+candidates_mail varchar(45) 
+candidates_gender varchar(45)
+
+values(1003,milna,22,female),
+ 
+ )
+ 
+ );
+
+    
+    
